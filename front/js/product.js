189e663596e3b49bb6782a30ce4e8ca712dc3bf0
Michaela Boctario <mbucatariu24@gmail.com>
2022-02-26 15:00:01 +0100

    add colors for js file for products page

--- front/js/product.js
+++ front/js/product.js
@@ -15,49 +15,53 @@ function getProductId()
 
 function showOneProduct(product) {     
     console.log(product);
-    let {_id, price, imageUrl, name, description, altTxt} = product; 
-    let stringContent = `
-    <article>
-    <div class="item__img">
-      <img src=${imageUrl} alt=${altTxt}
-    </div>
-    <div class="item__content">
-      <div class="item__content__titlePrice">
-        <h1 id="title">${name}</h1>
-        <p>Prix : <span id="price">${price}</span>€</p>
+    const {_id, price, colors, imageUrl, name, description, altTxt} = product; 
+    const stringContent = `<article>
+      <div class="item__img">
+        <img src=${imageUrl} alt=${altTxt}
       </div>
-
-      <div class="item__content__description">
-        <p class="item__content__description__title">Description :</p>
-        <p id="description">${description}</p>
-      </div>
-
-      <div class="item__content__settings">
-        <div class="item__content__settings__color">
-          <label for="color-select">Choisir une couleur :</label>
-          <select name="color-select" id="colors">
-              <option value="">--SVP, choisissez une couleur --</option>
-              <option value="vert">vert</option>
-              <option value="blanc">blanc</option>
-          </select>
+      <div class="item__content">
+        <div class="item__content__titlePrice">
+          <h1 id="title">${name}</h1>
+          <p>Prix : <span id="price">${price}</span>€</p>
         </div>
 
-        <div class="item__content__settings__quantity">
-          <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
-          <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
+        <div class="item__content__description">
+          <p class="item__content__description__title">Description :</p>
+          <p id="description">${description}</p>
         </div>
-      </div>
 
-      <div class="item__content__addButton">
-        <button id="addToCart">Ajouter au panier</button>
-      </div>
+        <div class="item__content__settings">
+          <div class="item__content__settings__color">
+            <label for="color-select">Choisir une couleur :</label>
+            <select name="color-select" id="colors">
+              <option value="">--SVP, choisissez une couleur --</option>            
+            </select>
+          </div>
 
-    </div>
-  </article>
-    `
+          <div class="item__content__settings__quantity">
+            <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
+            <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
+          </div>
+        </div>
+
+        <div class="item__content__addButton">
+          <button id="addToCart">Ajouter au panier</button>
+        </div>
+
+      </div>
+  </article>`;
+  
+    let stringColor = "";
+    for(let color of colors) {
+      stringColor += `<option value="${color}">${color}</option>`;
+    }
+
     let elementItem = document.getElementById("item");
     elementItem.innerHTML = stringContent;
-}
+    let elementItemColors = document.getElementById("colors");
+    elementItemColors.innerHTML += stringColor;
+  }
 
 function addOneProduct(productId) {
     fetch(`${host}/api/products/${productId}`)
