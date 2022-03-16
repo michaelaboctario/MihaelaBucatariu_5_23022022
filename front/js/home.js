a495a2393c988e252bf61a23ab83fac58658cbbc
Michaela Boctario <mbucatariu24@gmail.com>
2022-03-16 05:32:09 +0100

    update home.js use createelement

--- front/js/home.js
+++ front/js/home.js
@@ -1,46 +1,53 @@
 import { host } from './config.js';
 
 function showAllProducts(products) {
-    let stringContent="";
-    for(let product of products) {
-        console.log(product);
-        let {_id, imageUrl, name, description, altTxt} = product; 
-        stringContent += `<a href="./product.html?id=${_id}">
-            <article>
-            <img src="${imageUrl}" alt=${altTxt}>
-            <h3 class="productName">${name}</h3>
-            <p class="productDescription">${description}</p>
-            </article>
-        </a>`
-    }
-    let elementItems = document.getElementById("items");
-    elementItems.innerHTML += stringContent;
+  const itemsElement = document.getElementById("items");
+  for(let product of products) {
+      //console.log(product);
+      const {_id, imageUrl, name, description, altTxt} = product; 
+      
+      const productElement = document.createElement("a");
+      productElement.setAttribute("href", `./product.html?id=${_id}`);
+      const articleElement = document.createElement("article");
+      productElement.appendChild(articleElement);
+      const imgElement = document.createElement("img");
+      imgElement.setAttribute("src", imageUrl);
+      imgElement.setAttribute("alt", altTxt);
+      articleElement.appendChild(imgElement);
+      const h3Element = document.createElement("h3");
+      h3Element.className = "productName";
+      h3Element.textContent = name;
+      articleElement.appendChild(h3Element);
+      const pElement = document.createElement("p");
+      pElement.className = "productDescription";
+      pElement.textContent = description;
+      articleElement.appendChild(pElement);
+
+      itemsElement.appendChild(productElement);
+  }
+  
+  
 }
 
 function addProductsList() {
     fetch(`${host}/api/products`)
     .then(function(response) {
-        console.log(response);
+        //console.log(response);
         if(response.ok) {
           const res = response.json()
-          console.log(res);
+          //console.log(res);
           return res;
         } else {
           console.log('Mauvaise réponse du réseau');
         }
       })
       .then( function(response) {
-         console.log(response);
+         //console.log(response);
          showAllProducts(response);
       })
       .catch(function(error) {
         console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
       });
 }
-
-/* document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    addProductsList();
-}); */
 
 window.onload = addProductsList();
