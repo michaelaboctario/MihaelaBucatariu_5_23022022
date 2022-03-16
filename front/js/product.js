a07ee4189b82c9ab933f4002a915dd77a0ae4be2
Michaela Boctario <mbucatariu24@gmail.com>
2022-03-16 05:09:00 +0100

    update product.js use createelement

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,4 @@
-//import { host } from './config.js';
-const host = "http://localhost:3000";
+import { host } from './config.js';
 const cart=[];
 
 function getProductId()
@@ -15,59 +14,34 @@ function getProductId()
 }
 
 function showOneProduct(product) {     
-    console.log(product);
-    const {_id, price, colors, imageUrl, name, description, altTxt} = product; 
-    const stringContent = `<article>
-      <div class="item__img">
-        <img src=${imageUrl} alt=${altTxt}
-      </div>
-      <div class="item__content">
-        <div class="item__content__titlePrice">
-          <h1 id="title">${name}</h1>
-          <p>Prix : <span id="price">${price}</span>€</p>
-        </div>
-
-        <div class="item__content__description">
-          <p class="item__content__description__title">Description :</p>
-          <p id="description">${description}</p>
-        </div>
-
-        <div class="item__content__settings">
-          <div class="item__content__settings__color">
-            <label for="color-select">Choisir une couleur :</label>
-            <select name="color-select" id="colors">
-              <option value="">--SVP, choisissez une couleur --</option>            
-            </select>
-          </div>
-
-          <div class="item__content__settings__quantity">
-            <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
-            <input type="number" name="itemQuantity" min="1" max="100" value="1" id="quantity">
-          </div>
-        </div>
-        </div>
-        <div class="item__content__addButton">
-            <button id="addToCart">Ajouter au panier</button>
-        </div>
-    </article>`;
+  //console.log(product);
+  const {_id, price, colors, imageUrl, name, description, altTxt} = product; 
   
-    let stringColor = "";
-    for(let color of colors) {
-      stringColor += `<option value="${color}">${color}</option>`;
-    }
+  const imageProduct = document.createElement("img");
+  imageProduct.setAttribute("src", imageUrl);
+  imageProduct.setAttribute("alt", altTxt);
+  document.getElementsByClassName("item__img")[0].appendChild(imageProduct);
+  document.getElementById("title").textContent=name;
+  document.getElementById("price").textContent=price;
+  document.getElementById("description").textContent=description;
 
-    let elementItem = document.getElementById("item");
-    elementItem.innerHTML = stringContent;
-    let elementItemColors = document.getElementById("colors");
-    elementItemColors.innerHTML = stringColor;
-
-    const button = document.getElementById("addToCart")
-    
-    button.onclick = function() {
-      //console.log("click addToCart but");
-      addToCart();
-    }; 
+  const colorsElement =  document.getElementById("colors");
+  colorsElement.remove(colorsElement.firstChild);
+  for(let color of colors) {
+    const colorOption = document.createElement("option");
+    colorOption.setAttribute("value", color);
+    colorOption.textContent=color;
+    colorsElement.appendChild(colorOption);
   }
+  document.getElementById("quantity").value="1";
+
+  const button = document.getElementById("addToCart")  
+  button.onclick = function() {
+    //console.log("click addToCart but");
+    addToCart();
+    window.location.href = "cart.html";
+  }; 
+}
 
 function addToCart() {
     const productId = getProductId();
